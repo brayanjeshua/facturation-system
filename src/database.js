a901cef54deab165bc199600cb97ed7ead5b21c9
Brayan Jeshua <brayanjeshuavz@gmail.com>
2020-11-11 00:06:12 +0100

    Conchale ahora si al fin funciona

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
-import mongoose, { connect } from 'mongoose';
+const mongoose = require('mongoose');
 const URI = 'mongodb://localhost/mern-crud-test';
 
-connect(URI)
+mongoose.connect(URI)
   .then(db => console.log('Db is connected'))
   .catch(error => console.error(error));
-
-export default mongoose;
+  
+module.exports = mongoose;
